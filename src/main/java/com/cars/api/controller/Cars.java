f4ea504197a28bfc8e7be8a419b8ac6eeefa5705
nando-castro <fernando.castro10.master@gmail.com>
2023-01-23 15:53:15 -0300

    feat: finish project

--- src/main/java/com/cars/api/controller/Cars.java
+++ src/main/java/com/cars/api/controller/Cars.java
@@ -3,9 +3,19 @@ package com.cars.api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarDTO;
+import com.cars.api.model.Car;
+import com.cars.api.repository.CarRepository;
 
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,26 +23,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/cars")
 public class Cars {
 
-    String modelo;
-    String fabricante;
-    String dataFabricacao;
-    double valor;
-    int anoModelo;
+    @Autowired
+    private CarRepository repository;
 
     @PostMapping
-    public void createCar(@RequestBody CarDTO req) {
-        modelo = req.modelo();
-        fabricante = req.fabricante();
-        dataFabricacao = req.dataFabricacao();
-        valor = req.valor();
-        anoModelo = req.anoModelo();
-        System.out.println(req);
-        System.out.println(req.modelo());
-        System.out.println(req.anoModelo());
+    public void createCar(@RequestBody @Valid CarDTO req) {
+        repository.save(new Car(req));
     }
 
     @GetMapping
-    public String getCars() {
-        return "{ " + modelo + " / " + fabricante + " / " + dataFabricacao + " / " + valor + " / " + anoModelo + " }";
+    public List<Car> getCars(){
+        return repository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public void updateCar(@PathVariable Long id, @RequestBody CarDTO req){
+        repository.findById(id).map(car -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id){
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/cars/api/dto/CarDTO.java
+++ src/main/java/com/cars/api/dto/CarDTO.java
@@ -1,5 +1,13 @@
 package com.cars.api.dto;
 
-public record CarDTO(String modelo, String fabricante, String dataFabricacao, double valor, int anoModelo) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
+public record CarDTO(
+    @NotBlank @NotNull String modelo, 
+    @NotBlank @NotNull String fabricante,
+    @NotBlank @NotNull @Past String dataFabricacao, 
+    @NotBlank @NotNull double valor,
+    @NotBlank @NotNull int anoModelo) {
 }
